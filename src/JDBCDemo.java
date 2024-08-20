64ed10b78e0a4b2a9456acdd12b8a1d637f11190
kadir <kadirkayas2208@gmail.com>
2024-08-20 17:29:44 +0900

    update

--- src/JDBCDemo.java
+++ src/JDBCDemo.java
@@ -2,25 +2,34 @@ import java.sql.*;
 
 public class JDBCDemo {
     public static void main(String[] args) {
-        String url="jdbc:mysql://localhost:3306/jdbcdemo";
-        String username="root";
-        String password="";
-        try{
-            Class.forName("com.mysql.cj.jdbc.Driver");
-            Connection connection= DriverManager.getConnection(url,username,password);
-            Statement statement= connection.createStatement();
-            ResultSet resultSet=statement.executeQuery("Select * From student");
-            while(resultSet.next()){
-                System.out.println(resultSet.getInt(1)+ " " +
-                        resultSet.getString("name")+ " "+
+        String url = "jdbc:postgresql://kala.db.elephantsql.com:5432/iknbhltc";
+        String username = "iknbhltc";
+        String password = "8RZUarK1Z6HEYADkc5MTVxEYDGd-m8WE";
+
+        try {
+            // PostgreSQL JDBC driver'ını yükle
+            Class.forName("org.postgresql.Driver");
+
+            // Veritabanına bağlantı oluştur
+            Connection connection = DriverManager.getConnection(url, username, password);
+
+            // SQL sorgusu oluştur
+            Statement statement = connection.createStatement();
+            ResultSet resultSet = statement.executeQuery("SELECT * FROM student");
+
+            // Sonuçları işle
+            while (resultSet.next()) {
+                System.out.println(resultSet.getInt(1) + " " +
+                        resultSet.getString("name") + " " +
                         resultSet.getInt("age"));
             }
+
+            // Bağlantıyı kapat
             connection.close();
-        }
-        catch (ClassNotFoundException e) {
-            System.out.println("Driver not found: " + e);
+        } catch (ClassNotFoundException e) {
+            System.out.println("Driver not found: " + e.getMessage());
         } catch (SQLException e) {
-            System.out.println("SQL Exception: " + e);
+            System.out.println("SQL Exception: " + e.getMessage());
         }
     }
 }
